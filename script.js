9b7f65a265aa2737c85630ce8654e6f1a65a15e9
Student-pummykumari <147153862+Student-pummykumari@users.noreply.github.com>
2024-09-17 22:04:04 +0530

    Portfolio
    It's my portfolio.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  var typed4 = new leftsection('#typed4', {
+    strings: ['software engineering', 'Some HTML', 'Chars'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    attr: 'leftsection p1',
+    bindInputFocusEvents: true,
+    loop: true
+  });
+    
+
+  
